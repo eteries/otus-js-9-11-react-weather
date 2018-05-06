040a2d2f9d4d7d2e69a8d26b9e763eb7632a3c4b
eteries <internet.practica@gmail.com>
2018-05-06 16:42:10 +0300

    add api search

---
+++ cool-weather-app/src/components/Results.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Results.css';
+
+export default class Results extends Component {
+  render() {
+    if (!this.props.results.length) return null;
+    return (
+      <ul className="Results__ul">
+        {this.props.results.map(
+          (item) =>
+            <li key={item.id} className="Results__item">
+              <a className="Results__link" href={`/city/${item.id}`}>
+                {item.name}
+              </a>
+            </li>
+        )}
+      </ul>
+    );
+  }
+}
--- cool-weather-app/src/containers/SearchContainer.js
+++ cool-weather-app/src/containers/SearchContainer.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 import Search from '../components/Search';
+import Results from '../components/Results';
+import constants from '../constants';
+import axios from 'axios';
 
 export default class SearchContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      query: '',
+      results: []
+    };
+  }
+
+  handleUserInput = e => {
+    this.setState({
+      query: e.target.value
+    });
+    if (this.state.query.length > 2) {
+      this.getCities();
+    }
+  };
+
+  getCities = () => {
+    axios.get(`http://api.apixu.com/v1/search.json?key=${constants.KEY}&q=${this.state.query}`)
+      .then(({ data }) => {
+        this.setState({
+          results: data
+        })
+      });
+  };
+
   render() {
    return (
-     <Search placeholder="Find your city" search={e => console.log(e.target.value)}/>
+     <div>
+       <Search placeholder="Find your city" search={this.handleUserInput} />
+       <Results results={this.state.results} />
+     </div>
    )
   }
 }
